0397f5ee9335bba8257ed6073192c7f28129c324
DimBeam <pro100dima_2016@mail.ru>
2024-07-05 16:12:40 +1000

    Добавьте файлы проекта.

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using ToDoLab.Models;
+
+namespace ToDoLab.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private static List<ToDoItem> toDoItems = new List<ToDoItem>();
+
+        public ActionResult Index()
+        {
+            return View(toDoItems);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ToDoItem toDoItem)
+        {
+            toDoItems.Add(toDoItem);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var itemToDelete = toDoItems.FirstOrDefault(x => x.Id == id);
+            if (itemToDelete != null)
+                toDoItems.Remove(itemToDelete);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/ToDoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoLab.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Task { get; set; }
+    }
+}
